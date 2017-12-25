7150f89df715579bdacbe61dfa17101e2477bd87
unicorn808 <unicorncalcs@gmail.com>
2017-12-24 22:44:36 -1000

    loads data from cloud

--- extension/content/js/script.js
+++ extension/content/js/script.js
@@ -1,8 +1,10 @@
 var username = '';
-
+var noteTitle = 'Custom Title';
+var noteText = '';
 chrome.storage.sync.get(['username', 'enabled'], function(settings) {
 	if (settings.enabled === true) {
 		username = settings.username;
+		reqestNoteData();
 		enableButton();
 		$('#modal-text').on('input', function () {
 			console.log('Change Detected');
@@ -13,15 +15,36 @@ chrome.storage.sync.get(['username', 'enabled'], function(settings) {
 
 //Functions
 
+function reqestNoteData() {
+	$.ajax({
+		async: false,
+		method: 'post',
+		url: 'https://textedit-chrome.herokuapp.com/api/load-doc'
+		data: {
+			username: username,
+			url: window.location.href
+		},
+		success: function (res, err) {
+			console.log('Success!', res);
+			noteText = res.text;
+			noteTitle = res.title;
+		},
+		error: function (res, err) {
+			console.log('Error:', err);
+
+		}
+	})
+}
+
 
 function enableButton() {
 	$('body').append('<div id="textEditorWrapper" class="hoverable""></div>');
 	$('body').append('<div id="openModal" class="modalDialog" style="background-color: darkgray">\n' +
 		'    <div>' +
 		'		<a href="#close" title="Close" class="close">X</a>\n' +
-		'       <h2 id="modal-title" style="color: #000; display: inline-block;" contenteditable="true">Text Editor</h2> <h6 style="display: inline-block; color: #888888;" id="change-status">All Changes Saved</h6>\n' +
+		'       <h2 id="modal-title" style="color: #000; display: inline-block;" contenteditable="true">' + noteTitle + '</h2> <h6 style="display: inline-block; color: #888888;" id="change-status">All Changes Saved</h6>\n' +
 		'       <br>\n' +
-		'       <textarea id="modal-text" cols="12" rows="10" style="width: -webkit-fill-available; background-color: white"></textarea>\n' +
+		'       <textarea id="modal-text" placeholder="Some crazy note that morgan would be proud of!" cols="12" rows="10" style="width: -webkit-fill-available; background-color: white">' + noteText +'</textarea>\n' +
 		'    </div>\n' +
 		'</div>');
 	$('#textEditorWrapper').jqueryFab([{
@@ -30,6 +53,7 @@ function enableButton() {
 		"color": "#fffff",
 		"icon": "+"
 	}]);
+	$('#modal-title').html();
 	console.log('added button for user', username)
 }
 
